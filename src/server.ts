476010e7c4d3200b8e6db905e67135d894a4a436
macindex <machollanda@gmail.com>
2022-02-03 14:14:19 -0300

    first rout created

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express from "express"
 
 const app = express();
 
+// PATCH => Informação específica
+
+app.get("/test", (req, res) => {
+    return res.send("Hello NLW")
+})
+
 app.listen(3000, () => console.log("Server is running"));
