49f62c3767275c3bb777081c65408fade2b95d6d
macindex <machollanda@gmail.com>
2022-02-04 10:56:51 -0300

    database created

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express from "express"
+import "reflect-metadata"
+
+import "./database"
 
 const app = express();
 
-// PATCH => Informação específica
+// // PATCH => Informação específica
 
-app.get("/test", (req, res) => {
-    return res.send("Hello NLW")
-})
+// app.get("/test", (req, res) => {
+//     return res.send("Hello NLW")
+// })
 
-app.post("/test-post", (req, res) => {
-    return res.send("Olá post method")
-})
+// app.post("/test-post", (req, res) => {
+//     return res.send("Olá post method")
+// })
 
 app.listen(3000, () => console.log("Server is running"));
