b4c5f96bff480525d8f1804574fe87a80e882346
macindex <machollanda@gmail.com>
2022-02-06 13:40:03 -0300

    create entity CreateUsers

---
+++ src/database/migrations/1644165557196-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1644165557196 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
