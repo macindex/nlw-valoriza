f82bf21d075fb567bf85756d06c91348c70979b4
macindex <machollanda@gmail.com>
2022-03-16 14:58:09 -0300

    Service List user send compliments

--- src/services/ListUserSendComplimentsService.ts
+++ src/services/ListUserSendComplimentsService.ts
@@ -1,5 +1,18 @@
-class ListUserSendCompliments {
+import { getCustomRepository } from "typeorm"
+import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories"
+
+class ListUserSendComplimentsService {
+    async execute(user_id: string){
+        const complimentsRepositories = getCustomRepository(ComplimentsRepositories)
+
+        const compliments = await complimentsRepositories.find({
+            where: {
+                user_sender: user_id,
+            },
+        })
+        return compliments
+    } 
 
 }
 
-export { ListUserSendCompliments }
+export { ListUserSendComplimentsService }
