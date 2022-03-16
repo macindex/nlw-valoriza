36830ec5f9750981627dfd9cb59927320cd565a0
macindex <machollanda@gmail.com>
2022-03-16 14:47:46 -0300

    middleware ensureAuthenticated

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -19,7 +19,7 @@ export function ensureAuthenticated (request: Request, response: Response, next:
      //console.log(token)
     // Validar se o Token está válido
     try {
-        const { sub } = verify(authToken, "66c761f9ec0d72cc0d48dc795e0548627fb67411486c1f7b809854a752ba5f8e") as IPload;
+        const { sub } = verify(authToken, "2cc0d48dc795e0548627fb67411486c1") as IPload;
 
         request.user_id = sub
 
